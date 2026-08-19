16c23f57844b5b72e9bf975f279dce0cdef3eb07
Theredhawksings <myeoseul@gmail.com>
2026-08-19 16:09:58 +0900

    컨트롤러 변경

--- MyProject/Source/MyProject/MyProjectPlayerController.cpp
+++ MyProject/Source/MyProject/MyProjectPlayerController.cpp
@@ -13,6 +13,14 @@ void AMyProjectPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 메인 메뉴(UI 전용 입력 모드 + 마우스 커서 표시)를 거쳐 이 맵으로 넘어왔을 수 있으므로,
+	// 확실하게 게임 입력 모드로 되돌리고 커서를 숨긴다. 안 하면 WASD 가 게임으로 전달되지 않는다.
+	if (IsLocalPlayerController())
+	{
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+	}
+
 	// only spawn touch controls on local player controllers
 	if (ShouldUseTouchControls() && IsLocalPlayerController())
 	{
